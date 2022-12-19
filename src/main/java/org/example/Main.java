ffc3ebd786c26b53e9da930b4a37fb7073e98ca9
root <joosepkorela@gmail.com>
2022-12-19 21:08:03 +0200

    JBDC example

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+import controllers.Item;
+
+import static controllers.Item.getAllItems;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Item.getAllItems();
+       // Item.createNewItem();
+        //Item.getAllItems();
+        Item.updateItem();
+Item.getAllItems();
+    }
+
+}
